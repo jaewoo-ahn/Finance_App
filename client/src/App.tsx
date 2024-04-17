baddfe4398cdeaff7c98848b891d4bff2a00dd4b
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-17 20:44:40 +0900

    grid template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Box } from "@mui/system";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashborad";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -17,7 +18,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dasing board</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>prediction page</div>} />
             </Routes>
           </Box>
